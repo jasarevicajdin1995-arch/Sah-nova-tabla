dddc5f15ee981e43807f98b1ff6750715e5f0a16
jasarevicajdin1995-arch <jasarevic.ajdin1995@gmail.com>
2026-03-16 13:46:38 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -226,7 +226,7 @@ const promotionPieces = [];
 
 
 
-const BOARD_SCALE = 2.6170; // 2.4009 * 1.09
+const BOARD_SCALE = 2.6432; // 2.6170 * 1.01
 const BOARD_CENTER_X  = (GLB_X0 + GLB_X0 + 7 * GLB_XS) / 2;  // ≈ -0.09
 const BOARD_CENTER_Z  = (GLB_Z0 + GLB_Z0 + 7 * GLB_ZS) / 2;  // ≈ -0.70
 
